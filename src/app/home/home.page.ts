4d42e5891f3e014001c4a3597c5e7f40a5c180e0
Juan Santos <juanrscaba@gmail.com>
2024-09-19 20:44:39 -0400

    primer commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,46 @@
+import { Component, ElementRef, ViewChild, viewChild } from '@angular/core';
+import { LocationService } from '../services/location.service';
+import { GoogleMap } from '@capacitor/google-maps';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+ 
+  constructor(private locationSRV : LocationService) {
+    //Esta funcion toma la coordenada, pero en la web falla en android no xD
+   //this.getLocation();
+ 
+  }
+  
+  /*
+     lat:19.283997, 
+     lng:-69.188183
+  */
+ //Pones en las variables la coordenada que quieres mostrar
+  latitude : number = 19.283997
+  longitude :number = -69.188183
+
+  title = 'App Map'
+  position= {
+    lat:this.latitude, 
+    lng:this.longitude
+  }
+zoom = 10
+  label = {
+    color: 'blue',
+    text: 'Tu estas aqui'
+  }
+  getLocation() {
+    this.locationSRV.getPosition().then(pos => {
+        this.latitude = pos.lat;
+        this.longitude = pos.lng;
+    });
+
+    
+  }
+
+
+}
