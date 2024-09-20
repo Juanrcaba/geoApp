2cc2256c22b6d9291b960f4b40c9c973241c0679
Juan Santos <juanrscaba@gmail.com>
2024-09-19 22:04:11 -0400

    "asignacion de botones"

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -22,6 +22,7 @@ export class HomePage {
  //Pones en las variables la coordenada que quieres mostrar
   latitude : number = 19.283997
   longitude :number = -69.188183
+  mapTypeId :any = 'satellite'
 
   title = 'App Map'
   position= {
@@ -37,10 +38,17 @@ zoom = 10
     this.locationSRV.getPosition().then(pos => {
         this.latitude = pos.lat;
         this.longitude = pos.lng;
-    });
-
-    
+    });    
   }
 
+  changeMapMode(tipo:string){
+    if(tipo == 'satellite'){
+        this.mapTypeId = 'satellite'
+    }else if(tipo == 'roadmap'){
+      this.mapTypeId = 'roadmap'
+    }else if(tipo == 'terrain'){
+      this.mapTypeId = 'terrain'
+    }
+  }
 
 }
